04d12ec83835be227a24c096f6926303bd0ab35a
SejdiMurrja <hi.sejdimurrja@gmail.com>
2023-11-03 19:00:20 +0100

    flm

--- App2/models.py
+++ App2/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Planet(models.Model):
+    planet_id = models.AutoField(primary_key=True)
+    planet_title = models.CharField(max_length=200, null=True, blank=True)
+    planet_text = models.TextField(max_length=1000, null=True, blank=True)
+    planet_image = models.ImageField(upload_to="planet", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.planet_title}"
